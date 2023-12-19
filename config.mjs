68ea8f48536da72f08f9f0bbc97b488022413ab1
John Daly <john.daly2@gmail.com>
2023-12-19 10:50:26 -0800

    Demonstrate lack of alignment

--- config.mjs
+++ config.mjs
@@ -1,6 +1,8 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import { rspack } from "@rspack/core";
+import TerserPlugin from "terser-webpack-plugin";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const isRunningWebpack = !!process.env.WEBPACK;
@@ -25,6 +27,20 @@ const config = {
       ? path.resolve(__dirname, "webpack-dist")
       : path.resolve(__dirname, "rspack-dist"),
     filename: "[name].js",
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      isRunningRspack
+        ? new rspack.SwcJsMinimizerRspackPlugin({
+            extractComments: true,
+          })
+        : new TerserPlugin({
+            extractComments: {
+              banner: "This is a custom banner",
+            },
+          }),
+    ],
   },
   experiments: {
     css: true,
